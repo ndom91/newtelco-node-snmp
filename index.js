707ce8740590d03b5d4daf25ade3a5effcd69f46
ndo <yo@ni.co.de>
2019-12-16 15:19:02 +0100

    update: gather all data in array

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 #!/usr/bin/node
 
-const snmp = require('snmp-native')
-
-const session = new snmp.Session({ host: '192.168.11.223', community: 'n3wt3lco' })
-
 // /usr/bin/snmpwalk -t '1' -r '3' -v2c -c n3wt3lco -Pud -OQUsn -M /mnt/data/observium/mibs/rfc:/mnt/data/observium/mibs/net-snmp 'udp':'192.168.11.223':'161' .1.3.6.1.4.1.17095.6
 
-// const oid = '1.1.3.6.1.4.1.17095.6.1.0'
-// const oids = ['1.1.3.6.1.4.1.17095.6.2.0', '1.1.3.6.1.4.1.17095.6.1.0']
+const contactStatus = []
+const snmp = require('snmp-native')
+const session = new snmp.Session({ host: '192.168.11.223', community: 'n3wt3lco' })
 
 session.getSubtree({ oid: [1, 3, 6, 1, 4, 1, 17095, 6] }, function (error, varbinds) {
   if (error) {
     console.log('Fail :(')
   } else {
-    varbinds.forEach(function (vb) {
-      console.log(vb.oid + ' = ' + vb.value + ' (' + vb.type + ')')
-    })
+    // varbinds.forEach(function (vb) {
+    for (let i = 0; i < varbinds.length; i += 3) {
+      const vb1 = varbinds[i]
+      const vb2 = varbinds[i + 1]
+      //   const vb3 = varbinds[i + 2]
+
+      if (!vb1.value.includes('Undefine')) {
+        const sensorName = vb1.value
+        const sensorValue = vb2.value
+        contactStatus.push({ name: sensorName, value: sensorValue })
+      }
+    }
   }
+  console.log(JSON.stringify(contactStatus))
   session.close()
 })
